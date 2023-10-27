dee1938758482cec0b7d06d5d776bf54cdae1cd4
Julian <DaBobocrafter99@gmail.com>
2023-10-27 08:37:43 +0200

    testfile change

--- src/test/scala/de/htwg/poker/PokerSpec.scala
+++ src/test/scala/de/htwg/poker/PokerSpec.scala
@@ -4,9 +4,13 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class PokerSpec extends AnyWordSpec {
+  val hand = Hand()
+  val dealer = Dealer()
+  val playerName = "Julian"
+  val player = Player(playerName)
+
   "A Hand" should {
     "have valid cards when created" in {
-      val hand = Hand()
       assert(Card.contains(hand.Card1))
       assert(Card.contains(hand.Card2))
     }
@@ -14,7 +18,6 @@ class PokerSpec extends AnyWordSpec {
 
   "A Dealer" should {
     "have a hand with valid cards" in {
-      val dealer = Dealer()
       assert(Card.contains(dealer.Hand.Card1))
       assert(Card.contains(dealer.Hand.Card2))
     }
@@ -22,20 +25,15 @@ class PokerSpec extends AnyWordSpec {
 
   "A Player" should {
     "have a hand with valid cards" in {
-      val player = Player("Julian")
       assert(Card.contains(player.Hand.Card1))
       assert(Card.contains(player.Hand.Card2))
     }
     "have the name that was assigned to him" in {
-      val playerName = "Julian"
-      val player = Player(playerName)
       assert(player.Name == playerName)
     }
   }
   "The main method" should {
     "print out the cards of the dealer and the player" in {
-      val dealer = Dealer()
-      val player = Player("Julian")
       start should be(
         "Der Dealer zieht: " + dealer.Hand.toString + player.Name + " zieht: " + player.Hand.toString
       )

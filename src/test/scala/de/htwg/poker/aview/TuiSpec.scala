0976a1d53075c2187e7c2a6aca32f07d81481f3a
Radon-css <DaBobocrafter99@gmail.com>
2023-11-10 23:46:30 +0100

    ä

---
+++ src/test/scala/de/htwg/poker/aview/TuiSpec.scala
@@ -0,0 +1,10 @@
+package de.htwg.poker
+package aview
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+class TuiSpec extends AnyWordSpec {
+
+  "The TUI" should {}
+}
---
+++ src/test/scala/de/htwg/poker/controller/ControllerSpec.scala
@@ -0,0 +1,3 @@
+package de.htwg.poker.controller
+
+class ControllerSpec {}

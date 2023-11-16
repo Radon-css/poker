5820f8b2ca4e73796818d57b8cf968d6715d43e4
Julian <DaBobocrafter99@gmail.com>
2023-11-16 13:21:09 +0100

    Update ControllerSpec.scala

--- src/test/scala/de/htwg/poker/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/poker/controller/ControllerSpec.scala
@@ -1,3 +1,28 @@
 package de.htwg.poker.controller
 
 class ControllerSpec {}
+package de.htwg.poker.controller
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class ControllerSpec extends AnyWordSpec with Matchers {
+
+  "A Controller" when {
+
+    "created" should {
+
+      "have a GameState" in {
+        val gameState = new GameState
+        val controller = new Controller(gameState)
+        controller.gameState should be(gameState)
+      }
+
+      "be an Observable" in {
+        val gameState = new GameState
+        val controller = new Controller(gameState)
+        controller shouldBe an[Observable]
+      }
+    }
+  }
+}

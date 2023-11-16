a2a5287d34aa094d2f2d0ad87fcf84c67b6a0298
Julian <DaBobocrafter99@gmail.com>
2023-11-16 17:14:15 +0100

    Update tuiSpec.scala

--- src/test/scala/de/htwg/poker/aview/tuiSpec.scala
+++ src/test/scala/de/htwg/poker/aview/tuiSpec.scala
@@ -5,6 +5,8 @@ import org.scalatest._
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should._
 import de.htwg.poker.model.GameState
+import de.htwg.poker.model.shuffledDeck
+import de.htwg.poker.model.Player
 
 class TUISpec extends AnyWordSpec with Matchers {
 
@@ -15,13 +17,6 @@ class TUISpec extends AnyWordSpec with Matchers {
         val tui = new TUI(controller)
         tui.processInput("start 2") should be(true)
       }
-
-      "return true for valid bet command" in {
-        val controller = new Controller(new GameState(None, None, 0))
-        val tui = new TUI(controller)
-        tui.processInput("bet 50") should be(true)
-      }
-
       "return false for invalid command" in {
         val controller = new Controller(new GameState(None, None, 0))
         val tui = new TUI(controller)

bbf0bf8079408001b138564ee0208c1f6871dd2a
Julian <DaBobocrafter99@gmail.com>
2023-11-17 13:07:29 +0100

    Update GameStateSpec.scala

--- src/test/scala/de/htwg/poker/model/GameStateSpec.scala
+++ src/test/scala/de/htwg/poker/model/GameStateSpec.scala
@@ -36,5 +36,47 @@ class GameStateSpec extends AnyWordSpec with Matchers {
         updatedGameState.getPlayerAtTurn should be(1)
       }
     }
+    "folding" should {
+      "update the playerlist and move to the next player" in {
+        val player1 = Player(
+          Card(Suit.Spades, Rank.Ace),
+          Card(Suit.Hearts, Rank.King),
+          "Julian",
+          100
+        )
+        val player2 = Player(
+          Card(Suit.Diamonds, Rank.Queen),
+          Card(Suit.Clubs, Rank.Jack),
+          "Henrik",
+          100
+        )
+        val gameState =
+          GameState(Some(List(player1, player2)), Some(shuffledDeck))
+        val updatedGameState = gameState.fold()
+        updatedGameState.getPlayers should be(List(player2))
+        updatedGameState.getPlayerAtTurn should be(0)
+      }
+    }
+    "calling" should {
+      "call the right amount and update the player's coins" in {
+        val player1 = Player(
+          Card(Suit.Spades, Rank.Ace),
+          Card(Suit.Hearts, Rank.King),
+          "Julian",
+          100
+        )
+        val player2 = Player(
+          Card(Suit.Diamonds, Rank.Queen),
+          Card(Suit.Clubs, Rank.Jack),
+          "Henrik",
+          100
+        )
+        val gameState =
+          GameState(Some(List(player1, player2)), Some(shuffledDeck))
+        val updatedGameState = gameState.bet(50)
+        val updatedGameState2 = gameState.call()
+        updatedGameState2.getPlayers(1).coins should be(50)
+      }
+    }
   }
 }

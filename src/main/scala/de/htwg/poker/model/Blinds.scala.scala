99d8a13f4913b2e308fe070a9a2dc38687414bfa
HenrikFrs <henrik.freisleben@gmail.com>
2023-11-30 18:16:04 +0100

    Delete Blinds.scala.scala

--- src/main/scala/de/htwg/poker/model/Blinds.scala.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.poker.model
-
-abstract class Blind(blindAmount: Int) {
-    val amount: Int = blindAmount
-}
-
-class smallBlind(smallBlindAmount: Int) extends Blind(smallBlindAmount)
-class bigBlind(bigBlindAmount: Int) extends Blind(bigBlindAmount)

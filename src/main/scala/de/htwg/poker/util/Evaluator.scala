977e5954dfc2b1d1ccdc8a9e62f9a72c62fec146
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2023-12-22 12:46:56 +0100

    Update Evaluator.scala

--- src/main/scala/de/htwg/poker/util/Evaluator.scala
+++ src/main/scala/de/htwg/poker/util/Evaluator.scala
@@ -1,7 +1,43 @@
 package de.htwg.poker.util
 
-import de.htwg.poker.model.Player
+import de.htwg.poker.model.*
 
-def evaluate(playerList: List[Player], board: List[Card]): Player = {
+class Evaluator(gameState: GameState) {
 
-}
+    enum Type:
+        case High, Pair, DoublePair, Triples, Straight, Flush, FullHouse, Quads, StraightFlush, RoyalFlush
+        def strength: Int = this match {
+            case High => 1
+            case Pair => 2
+            case DoublePair => 3
+            case Triples => 4
+            case Straight => 5
+            case Flush => 6
+            case FullHouse => 7
+            case Quads => 8
+            case StraightFlush => 9
+            case RoyalFlush => 10
+    }    
+   val boardCards: List[Card] = gameState.getBoard
+   val playerCards: Map[Player, (Card,Card)] = gameState.getPlayers.map(player => player -> (player.card1,player.card2)).toMap
+
+    def evaluate(): Player = {
+        val bestPlayerHands: Map[Player, List[Card]] = playerCards.map {case (player,(card1,card2)) => player -> evalHand((card1,card2))}
+        evalWinner(bestPlayerHands)
+    }
+
+    def EvalHand(cards: (Card, Card)): List[Card] = {
+        val (card1, card2) = cards
+
+        val rankHistogramm: List[(Rank,Int)] = boardCards.map(_.rank).groupBy(identity).mapValues(_.size).toList.sortBy(-_._2)
+
+        if (rankHistogramm(0)._2 == 4 && rankHistogramm(1)._2 == 1) 
+            
+
+    }
+
+    def evalWinner(playerBestHands: Map[Player, List[Card]]): Player = {
+
+    }
+
+    }

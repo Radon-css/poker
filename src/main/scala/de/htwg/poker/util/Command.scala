c9dc5b641bf4729db3478c4bc12f7499797dda19
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2023-12-22 11:19:39 +0100

    evaluator

--- src/main/scala/de/htwg/poker/util/Command.scala
+++
@@ -1,7 +0,0 @@
-//package de.htwg.poker.util
-
-/*trait Command {
-  def doStep: Unit
-  def undoStep: Unit
-  def redoStep: Unit
-}*/
---
+++ src/main/scala/de/htwg/poker/util/Evaluator.scala
@@ -0,0 +1,7 @@
+package de.htwg.poker.util
+
+import de.htwg.poker.model.Player
+
+def evaluate(playerList: List[Player], board: List[Card]): Player = {
+
+}

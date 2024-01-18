335fbb1d2eb80e1109bd7428a60581c8dacf31a2
Radon-css <DaBobocrafter99@gmail.com>
2024-01-18 21:15:36 +0100

    Update GameStateSpec.scala

--- src/test/scala/de/htwg/poker/model/GameStateSpec.scala
+++ src/test/scala/de/htwg/poker/model/GameStateSpec.scala
@@ -1,1 +1,22 @@
+package de.htwg.poker.model
 
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class GameStateSpec extends AnyWordSpec with Matchers {
+  "A GameState" when {
+    "created" should {
+      "have the correct initial values" in {
+        val gameState =
+          new GameState(List(), None, None, 0, 0, List(), 0, 10, 20, 0, false)
+
+        gameState.getPlayers should be(List())
+        gameState.getDeck should be(List())
+        gameState.getPot should be(0)
+        gameState.getPlayerAtTurn should be(0)
+        gameState.getOriginalPlayers should be(List())
+        gameState.getBoard should be(List())
+      }
+    }
+  }
+}

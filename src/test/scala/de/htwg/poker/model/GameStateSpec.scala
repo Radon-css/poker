112242c12bc4726db486d57d7ebff613364227df
Radon-css <DaBobocrafter99@gmail.com>
2024-01-18 22:29:29 +0100

    s

--- src/test/scala/de/htwg/poker/model/GameStateSpec.scala
+++ src/test/scala/de/htwg/poker/model/GameStateSpec.scala
@@ -20,19 +20,76 @@ class GameStateSpec extends AnyWordSpec with Matchers {
       }
     }
   }
-  "Update Board" when {
-    /*
-    "called" should {
-      "update the board" in {
-        val gameState =
-          new GameState(List(), None, None, 0, 0, List(), 0, 10, 20, 0, false)
-        val board = List(Card(Suit.Spades, Rank.Ace))
-        val newGameState = gameState.UpdateBoard(board)
-        newGameState.getBoard should be(board)
-      }
+  "startRound" should {
+    "initialize a new round with correct values" in {
+      val gameState = new GameState(
+        List(
+          Player(
+            new Card(Suit.Hearts, Rank.Ace),
+            new Card(Suit.Hearts, Rank.King),
+            "Player1",
+            100
+          ),
+          Player(
+            new Card(Suit.Spades, Rank.Ace),
+            new Card(Suit.Spades, Rank.King),
+            "Player2",
+            100
+          ),
+          Player(
+            new Card(Suit.Diamonds, Rank.Ace),
+            new Card(Suit.Diamonds, Rank.King),
+            "Player3",
+            100
+          )
+        ),
+        None,
+        None,
+        0,
+        0,
+        List(),
+        0,
+        10,
+        20,
+        0,
+        false
+      )
+
+      val newGameState = gameState.UpdateBoard.startRound
+
+      newGameState.getPlayers.size should be(3)
+      newGameState.getDeck.size should be(46)
+      newGameState.getPlayerAtTurn should be(2)
+      newGameState.getHighestBetSize should be(20)
+      newGameState.getBoard should be(Nil)
+      newGameState.getPot should be(30)
+      newGameState.getSmallBlind should be(10)
+      newGameState.getBigBlind should be(20)
+      newGameState.getSmallBlindPointer should be(0)
+
+      val player1 = newGameState.getPlayers.head
+      player1.card1 should not be null
+      player1.card2 should not be null
+      player1.playername should be("Player1")
+      player1.balance should be(90)
+      player1.currentAmountBetted should be(10)
+
+      val player2 = newGameState.getPlayers(1)
+      player2.card1 should not be null
+      player2.card2 should not be null
+      player2.playername should be("Player2")
+      player2.balance should be(80)
+      player2.currentAmountBetted should be(20)
+
+      val player3 = newGameState.getPlayers(2)
+      player3.card1 should not be null
+      player3.card2 should not be null
+      player3.playername should be("Player3")
+      player3.balance should be(100)
+      player3.currentAmountBetted should be(0)
     }
-     */
   }
+
   "getNextPlayer" should {
     "return the next player index" in {
       val gameState =
--- src/test/scala/de/htwg/poker/util/UndoSpec.scala
+++ src/test/scala/de/htwg/poker/util/UndoSpec.scala
@@ -1,1 +1,33 @@
+package de.htwg.poker.util
 
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.mockito.Mockito._
+/*
+import de.htwg.poker.controller.Controller
+import de.htwg.poker.model.GameState
+
+class UndoSpec extends AnyWordSpec with Matchers {
+  "An UndoManager" when {
+    "performing undo and redo steps" should {
+      "correctly update the game state" in {
+        val controller = mock(classOf[Controller])
+        val undoManager = new UndoManager()
+        val gameState1 = mock(classOf[GameState])
+        val gameState2 = mock(classOf[GameState])
+        val gameState3 = mock(classOf[GameState])
+
+        undoManager.doStep(gameState1)
+        undoManager.doStep(gameState2)
+        undoManager.doStep(gameState3)
+
+        undoManager.undoStep(controller, gameState2)
+        verify(controller).gameState_=(gameState2)
+
+        undoManager.redoStep(controller)
+        verify(controller).gameState_=(gameState3)
+      }
+    }
+  }
+}
+ */

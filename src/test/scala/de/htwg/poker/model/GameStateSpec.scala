b44a45f81e52d0dbc52f78e7f69889cb7a9e82b4
Radon-css <DaBobocrafter99@gmail.com>
2024-01-18 21:48:24 +0100

    Update GameStateSpec.scala

--- src/test/scala/de/htwg/poker/model/GameStateSpec.scala
+++ src/test/scala/de/htwg/poker/model/GameStateSpec.scala
@@ -19,4 +19,66 @@ class GameStateSpec extends AnyWordSpec with Matchers {
       }
     }
   }
+  "Update Board" when {
+    /*
+    "called" should {
+      "update the board" in {
+        val gameState =
+          new GameState(List(), None, None, 0, 0, List(), 0, 10, 20, 0, false)
+        val board = List(Card(Suit.Spades, Rank.Ace))
+        val newGameState = gameState.UpdateBoard(board)
+        newGameState.getBoard should be(board)
+      }
+    }
+     */
+  }
+  "getNextPlayer" should {
+    "return the next player index" in {
+      val gameState =
+        new GameState(List(), None, None, 0, 0, List(), 0, 10, 20, 0, false)
+      gameState.getNextPlayer should be(1)
+    }
+    "return 1 if the current player is the last player" in {
+      val gameState =
+        new GameState(List(), None, None, 0, 1, List(), 0, 10, 20, 0, false)
+      gameState.getNextPlayer should be(1)
+    }
+  }
+  "getNextPlayerWhenFold" should {
+    "return the next player index" in {
+      val gameState =
+        new GameState(List(), None, None, 0, 0, List(), 0, 10, 20, 0, false)
+      gameState.getNextPlayerWhenFold should be(0)
+    }
+    "return the current player index if the current player is the last player" in {
+      val gameState =
+        new GameState(List(), None, None, 0, 1, List(), 0, 10, 20, 0, false)
+      gameState.getNextPlayerWhenFold should be(0)
+    }
+  }
+  "getPreviousPlayer" should {
+    "return the previous player index" in {
+      val gameState =
+        new GameState(List(), None, None, 1, 1, List(), 0, 10, 20, 0, false)
+      gameState.getPreviousPlayer should be(0)
+    }
+  }
+  "getCurrentHand" should {
+    /*
+    "return the evaluated hand" in {
+      val gameState =
+        new GameState(List(), None, None, 0, 0, List(), 0, 10, 20, 0, false)
+      val evaluator = new Evaluator()
+      val expectedHand = evaluator.evaluate(
+        List(
+          gameState.getPlayers(gameState.getPlayerAtTurn).card1,
+          gameState.getPlayers(gameState.getPlayerAtTurn).card2
+        ),
+        gameState.getBoard
+      )
+      gameState.getCurrentHand should be(expectedHand)
+    }
+  }
+     */
+  }
 }

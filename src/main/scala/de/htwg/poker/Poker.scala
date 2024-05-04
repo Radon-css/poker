638e27a565f31b09783d1f70a775190d8c18dafd
HenrikFrs <henrik.freisleben@gmail.com>
2024-05-04 21:04:25 +0200

    deleted tui comments

--- src/main/scala/de/htwg/poker/Poker.scala
+++ src/main/scala/de/htwg/poker/Poker.scala
@@ -1,6 +1,5 @@
 package de.htwg.poker;
 import controller.Controller
-//import aview.TUI
 import scala.io.StdIn.readLine
 import model.GameState
 import aview.GUI
@@ -14,19 +13,12 @@ object Poker {
   val controller = new Controller(
     new GameState(Nil, None, None, 0, 0, Nil, 0, 0, 0, 0)
   )
-  // val tui = new TUI(controller)
   val gui = new GUI(controller)
 
   @main
   def run: Unit = {
     // read and save Hashes for WinnerEvaluation
     Evaluator.readHashes
-    // the strange looking future and await stuff is needed so we can run our GUI and TUI concurrently
-    // implicit val context = scala.concurrent.ExecutionContext.global
-    // val f = Future {
     gui.main(Array[String]())
-    // }
-    // tui.gameLoop()
-    // Await.ready(f, scala.concurrent.duration.Duration.Inf)
   }
 }

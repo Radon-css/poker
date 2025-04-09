32513862beda0edd5d592d38bc1552189f012e14
Julian <Julianschmuck@outlook.de>
2025-04-09 10:29:34 +0200

    Moved Poker.scala

---
+++ controllerService/src/main/scala/Poker.scala
@@ -0,0 +1,32 @@
+package de.htwg.poker;
+import controller.Controller
+import aview.TUI
+import scala.io.StdIn.readLine
+import model.GameState
+import aview.GUI
+import de.htwg.poker.util.Evaluator
+
+import scala.concurrent.Await
+import scala.concurrent.Future
+
+object Poker {
+  // here we need to pass in a stub of a GameState in order to initially create our controller
+  val controller = new Controller(
+    new GameState(Nil, None, None, 0, 0, Nil, 0, 0, 0, 0)
+  )
+  val tui = new TUI(controller)
+  val gui = new GUI(controller)
+
+  @main
+  def run: Unit = {
+    // read and save Hashes for WinnerEvaluation
+    Evaluator.readHashes
+    // the strange looking future and await stuff is needed so we can run our GUI and TUI concurrently
+    implicit val context = scala.concurrent.ExecutionContext.global
+    val f = Future {
+      gui.main(Array[String]())
+    }
+    tui.gameLoop()
+    Await.ready(f, scala.concurrent.duration.Duration.Inf)
+  }
+}
---
+++ controllerService/src/main/scala/aview/tui.scala
@@ -0,0 +1,104 @@
+package de.htwg.poker
+package aview
+import controller.Controller
+import util.Observer
+import scala.io.StdIn.readLine
+import scala.util.{Try, Success, Failure}
+
+class TUI(controller: Controller) extends Observer {
+  controller.add(this)
+
+  override def update: Unit = {
+    println(controller.toString)
+  }
+
+  def gameLoop(): Unit = {
+    while (true) {
+      val input = readLine()
+      processInput(input)
+    }
+  }
+
+  def processInput(input: String): Boolean = {
+    val inputList = input.split(" ").toList
+    inputList(0) match {
+      /* start a game by typing start followed by the name of the players participating and finally the amount of the small Blind and big Blind
+        (Blinds are the cost for the players to participate in the next round).
+         for example, type "start player1 player2 player3 player4 10 20"*/
+      case "start" =>
+        val result: Try[Boolean] = Try(
+          controller.createGame(
+            inputList.tail.dropRight(2),
+            inputList.init.last,
+            inputList.last
+          )
+        )
+        result match {
+          case Success(value)     => return true
+          case Failure(exception) => println(s"Error: ${exception.getMessage}")
+        }
+        false
+      // start a quick example game without inserting playernames and so on.
+      case "x" =>
+        val result: Try[Boolean] = Try(
+          controller.createGame(
+            List("Henrik", "Julian", "Till", "Julian2", "Dominik", "Luuk"),
+            "10",
+            "20"
+          )
+        )
+        result match {
+          case Success(value)     => return true
+          case Failure(exception) => println(s"Error: ${exception.getMessage}")
+        }
+        false
+      // make a monetary contribution to the pot by calling "bet (amount)". This indicates that you are confident in your current hand.
+      case "bet" =>
+        val result: Try[Boolean] = Try(controller.bet(inputList(1).toInt))
+        result match {
+          case Success(value)     => return true
+          case Failure(exception) => println(s"Error: ${exception.getMessage}")
+        }
+        false
+      // fold to to discard your hand and forfeit any further involvement in the current round. Do this if your hand is weak.
+      case "fold" =>
+        val result: Try[Boolean] = Try(controller.fold)
+        result match {
+          case Success(value)     => return true
+          case Failure(exception) => println(s"Error: ${exception.getMessage}")
+        }
+        false
+      // call to match the current bet to continue in the round and see the next community cards. You can only call if there were bets beforehand.
+      case "call" =>
+        val result: Try[Boolean] = Try(controller.call)
+        result match {
+          case Success(value)     => return true
+          case Failure(exception) => println(s"Error: ${exception.getMessage}")
+        }
+        false
+      // check to dismiss your right to bet and pass the action to the next player. You can only do this if there were no bets in the current state of the game.
+      case "check" =>
+        val result: Try[Boolean] = Try(controller.check)
+        result match {
+          case Success(value)     => return true
+          case Failure(exception) => println(s"Error: ${exception.getMessage}")
+        }
+        false
+      // close the game
+      case "q" =>
+        sys.exit()
+        true
+      // undo the last move
+      case "u" =>
+        controller.undo
+        true
+      // redo the move
+      case "r" =>
+        controller.redo
+        true
+      case _ =>
+        println("invalid command")
+        false
+    }
+  }
+}

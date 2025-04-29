18e0874fdfc78f726d35d423869049bc413ef9a6
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2025-04-29 19:49:08 +0200

    .

--- coreService/src/main/scala/coreRoutes.scala
+++ coreService/src/main/scala/coreRoutes.scala
@@ -7,6 +7,8 @@ import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
 import de.htwg.poker.controllers.Receiver
 import scala.concurrent.ExecutionContext
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.model.HttpMethods._
 
 class CoreRoutes {
   implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "PokerSystem")
@@ -15,59 +17,78 @@ class CoreRoutes {
 
   val receiver = new Receiver()(system, mat)
 
-  val routes: Route = pathPrefix("core") {
-    concat(
-      path("get") {
+  def corsHandler(route: Route): Route = {
+    respondWithHeaders(
+      `Access-Control-Allow-Origin`(HttpOrigin("http://localhost:5173")),
+      `Access-Control-Allow-Credentials`(true),
+      `Access-Control-Allow-Headers`("Content-Type", "Authorization", "X-Requested-With", "playerid"),
+      `Access-Control-Allow-Methods`(GET, POST, OPTIONS)
+    ) {
+      options {
+        complete("")
+      } ~ route
+    }
+  }
+
+  val routes: Route = corsHandler { 
+    pathPrefix("core") {
+      concat(
+        path("get") {
+          get {
+            receiver.getJson // ruft deine Funktion auf
+          }
+        },
+        path("bet" / IntNumber) { x =>
+          post {
+            receiver.bet(x)
+          }
+        },
+        path("allIn") {
+          post {
+            receiver.allIn()
+          }
+        },
+        path("fold") {
+          post {
+            receiver.fold()
+          }
+        },
+        path("call") {
+          post {
+            receiver.call()
+          }
+        },
+        path("check") {
+          post {
+            receiver.check()
+          }
+        },
+        path("restartGame") {
+          post {
+            receiver.restartGame()
+          }
+        },
+        path("leave") {
+          post {
+            receiver.leave()
+          }
+        },
+        path("join") {
+          get {
+            receiver.join()
+          }
+        },
+        path("newGame") {
+          post {
+            receiver.newGame()
+          }
+        },
+        path("websocket") {
         get {
-          receiver.getJson // ruft deine Funktion auf
-        }
-      },
-      path("bet" / IntNumber) { x =>
-        post {
-          receiver.bet(x)
-        }
-      },
-      path("allIn") {
-        post {
-          receiver.allIn()
-        }
-      },
-      path("fold") {
-        post {
-          receiver.fold()
-        }
-      },
-      path("call") {
-        post {
-          receiver.call()
-        }
-      },
-      path("check") {
-        post {
-          receiver.check()
-        }
-      },
-      path("restartGame") {
-        post {
-          receiver.restartGame()
-        }
-      },
-      path("leave") {
-        post {
-          receiver.leave()
-        }
-      },
-      path("join") {
-        get {
-          receiver.join()
-        }
-      },
-      path("newGame") {
-        post {
-          receiver.newGame()
+          receiver.socket()
         }
       }
-      // WebSocket können wir auch bauen, wenn du willst
-    )
+      )
+    }
   }
 }

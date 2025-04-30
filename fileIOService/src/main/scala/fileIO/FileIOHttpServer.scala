e60e2dcef8d5b2d13c04267dfc1ae8fa3c181522
Julian <Julianschmuck@outlook.de>
2025-04-30 11:01:12 +0200

    FileIOServer

---
+++ fileIOService/src/main/scala/fileIO/FileIOHttpServer.scala
@@ -0,0 +1,60 @@
+package de.htwg.poker.fileIO
+
+import akka.Done
+import akka.actor.{ActorSystem, ClassicActorSystemProvider, CoordinatedShutdown}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.server.Route
+import org.slf4j.LoggerFactory
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object FileIOHttpServer {
+  private val logger = LoggerFactory.getLogger(getClass.getName.init)
+
+  def run(
+      system: ActorSystem,
+      ec: ExecutionContext,
+      port: Int = 8084
+  ): Future[ServerBinding] = {
+    val routes = new FileIORoutes().routes
+    val host = "0.0.0.0"
+
+    implicit val classicSystem: ClassicActorSystemProvider = system
+    implicit val executionContext: ExecutionContext = ec
+
+    val serverBinding = Http()
+      .newServerAt(host, port)
+      .bind(routes)
+
+    CoordinatedShutdown(system).addTask(
+      CoordinatedShutdown.PhaseServiceStop,
+      "shutdown-server"
+    ) { () =>
+      shutdown(serverBinding)(system, ec)
+    }
+
+    serverBinding.onComplete {
+      case Success(binding) =>
+        logger.info(s"FileIO Service running at http://${binding.localAddress.getHostString}:${binding.localAddress.getPort}/")
+      case Failure(ex) =>
+        logger.error("FileIO Service failed to start", ex)
+        system.terminate()
+    }
+
+    serverBinding
+  }
+
+  private def shutdown(
+      serverBinding: Future[ServerBinding]
+  )(implicit system: ActorSystem, ec: ExecutionContext): Future[Done] = {
+    serverBinding.flatMap { binding =>
+      binding.unbind().map { _ =>
+        logger.info("Shutting down TUI Service...")
+        system.terminate()
+        Done
+      }
+    }
+  }
+}
---
+++ fileIOService/src/main/scala/fileIO/FileIOService.scala
@@ -0,0 +1,16 @@
+package de.htwg.poker.fileIO
+
+import akka.actor.ActorSystem
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+object FileIOService {
+  @main def startFileIOServer: Unit = {
+    implicit val system: ActorSystem = ActorSystem("PokerFileIOService")
+    implicit val ec: ExecutionContext = system.dispatcher
+
+    FileIOHttpServer.run(system, ec, port = 8082)
+
+    Await.result(Future.never, Duration.Inf)
+  }
+}

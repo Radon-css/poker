bb8d8af13973eee957dd6efbaf240e0600836835
Radon-css <Julianschmuck@outlook.de>
2025-05-05 20:31:37 +0200

    Deployed database to koyeb

--- dbService/src/main/scala/db/dbImpl/slickImpl/DatabaseConfig.scala
+++ dbService/src/main/scala/db/dbImpl/slickImpl/DatabaseConfig.scala
@@ -1,10 +1,10 @@
 package de.htwg.poker.db.dbImpl.slickImpl
 
 object DatabaseConfig {
-  val DB_POSTGRES_URL: String = "jdbc:postgresql://db:5432/pokerdb"
+  val DB_POSTGRES_URL: String = "jdbc:postgresql://ep-dry-dust-a2v3ufk8.eu-central-1.pg.koyeb.app/koyebdb?user=koyeb-adm&password=npg_W3Ky7hxguGVQ"
   //val DB_POSTGRES_USER: String = sys.env("POSTGRES_USER")
-  val DB_POSTGRES_USER: String = "pokeruser"
+  val DB_POSTGRES_USER: String = "koyeb-adm"
   //val DB_POSTGRES_PASS: String = sys.env("POSTGRES_PASSWORD")
-  val DB_POSTGRES_PASS: String = "pokerpassword"
+  val DB_POSTGRES_PASS: String = "npg_W3Ky7hxguGVQ"
   val DB_POSTGRES_DRIVER: String = "org.postgresql.Driver"
 }

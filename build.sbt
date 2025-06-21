8451224cfc1c21211cc05c41bff9d197cdbdb435
Radon-css <Julianschmuck@outlook.de>
2025-06-21 20:14:39 +0200

    fixed client and build.sbt

--- build.sbt
+++ build.sbt
@@ -1,56 +1,43 @@
 val scala3Version = "3.6.4"
+val scalatestVersion = "3.2.14"
+val akkaVersion = "2.8.5"
+val akkaHttpVersion = "10.5.3"
+val circeVersion = "0.14.1"
+val jacksonVersion = "2.15.3"
+
+
 
 ThisBuild / version := "1.0.1"
 ThisBuild / scalaVersion := scala3Version
-ThisBuild / scalacOptions += "-Xmax-inlines:64"
-Compile / mainClass := Some("de.htwg.poker.CoreServer")
-Compile / resourceDirectory := baseDirectory.value / "src" / "main" / "resources"
-
-val akkaVersion = "2.8.5"
-val akkaHttpVersion = "10.5.3"
-
-
-
 
 ThisBuild / libraryDependencies ++= Seq(
-  "org.scalactic" %% "scalactic" % "3.2.14",
-  "org.scalatest" %% "scalatest" % "3.2.14" % "test",
+  "org.scalactic" %% "scalactic" % scalatestVersion,
+  "org.scalatest" %% "scalatest" % scalatestVersion % Test,
   "org.scala-lang.modules" %% "scala-swing" % "3.0.0" cross CrossVersion.for3Use2_13,
-
-  // Akka dependencies - all with cross version
-  "com.typesafe.akka" %% "akka-actor" % akkaVersion cross CrossVersion.for3Use2_13,
+  "org.scala-lang.modules" %% "scala-xml" % "2.1.0",
+  "com.typesafe.play" %% "play-json" % "2.10.0-RC5",
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion,
+  "org.scalatestplus" %% "mockito-5-12" % "3.2.19.0" % Test,
+  "ch.qos.logback" % "logback-classic" % "1.5.2",
+  "net.logstash.logback" % "logstash-logback-encoder" % "7.4",
   "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion cross CrossVersion.for3Use2_13,
-  "com.typesafe.akka" %% "akka-protobuf-v3" % akkaVersion cross CrossVersion.for3Use2_13,
   "com.typesafe.akka" %% "akka-stream" % akkaVersion cross CrossVersion.for3Use2_13,
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion cross CrossVersion.for3Use2_13,
-  "com.typesafe.akka" %% "akka-serialization-jackson" % akkaVersion cross CrossVersion.for3Use2_13,
-  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion cross CrossVersion.for3Use2_13,
-  
-  // Other dependencies that need cross version
-  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.15.2" cross CrossVersion.for3Use2_13,
-  "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.2" cross CrossVersion.for3Use2_13,
-  "com.typesafe" %% "ssl-config-core" % "0.6.1" cross CrossVersion.for3Use2_13,
-
-  // Rest of your dependencies...
-  "io.circe" %% "circe-core" % "0.14.1",
-  "io.circe" %% "circe-generic" % "0.14.1",
-  "io.circe" %% "circe-parser" % "0.14.1",
-
-  "com.typesafe.play" %% "play-json" % "2.10.0-RC5" cross CrossVersion.for3Use2_13,
-  "com.typesafe.play" %% "play" % "2.9.0" cross CrossVersion.for3Use2_13,
-
-  "org.slf4j" % "slf4j-api" % "2.0.7",
-  "ch.qos.logback" % "logback-classic" % "1.4.7",
-
+  "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test cross CrossVersion.for3Use2_13,
+  "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test cross CrossVersion.for3Use2_13,
+  "org.wiremock" % "wiremock" % "3.12.1" % Test,
   "com.typesafe.slick" %% "slick" % "3.6.0" cross CrossVersion.for3Use2_13,
   "org.postgresql" % "postgresql" % "42.7.3",
-
+  "com.h2database" % "h2" % "2.3.232",
   "org.mongodb.scala" %% "mongo-scala-driver" % "5.4.0" cross CrossVersion.for3Use2_13,
-
+  "io.gatling.highcharts" % "gatling-charts-highcharts" % "3.13.5" % Test,
+  "io.gatling" % "gatling-test-framework" % "3.13.5" % Test,
   "org.apache.kafka" % "kafka-clients" % "4.0.0",
   "org.apache.kafka" %% "kafka-streams-scala" % "3.7.0" cross CrossVersion.for3Use2_13,
   "com.typesafe.akka" %% "akka-stream-kafka" % "4.0.2" cross CrossVersion.for3Use2_13,
-  "org.scala-lang.modules" %% "scala-xml" % "2.1.0" cross CrossVersion.for3Use2_13
+  "com.github.danielwegener" % "logback-kafka-appender" % "0.2.0-RC2"
 )
 
 /*libraryDependencies ++= {
--- coreService/src/main/scala/Client.scala
+++ coreService/src/main/scala/Client.scala
@@ -1,23 +1,237 @@
-package.
+package de.htwg.poker
 
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import akka.Done
+import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, CoordinatedShutdown}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.Materializer
+import de.htwg.poker.model._
+import de.htwg.poker.model.{Card, GameState, Player}
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-object KafkaConfig {
-  val bootstrapServers = "localhost:9092"
+object Client {
 
-  def producerSettings: Map[String, Object] = Map(
-    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> bootstrapServers,
-    ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer],
-    ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG -> classOf[StringSerializer]
-  )
+  import AppConfig.{materializer, system}
 
-  def consumerSettings(groupId: String): Map[String, Object] = Map(
-    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> bootstrapServers,
-    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-    ConsumerConfig.GROUP_ID_CONFIG -> groupId,
-    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest"
-  )
+  def calcWinner(
+      gameState: GameState
+  ): Future[List[Player]] = {
+
+    val jsonString = gameState.asJson.noSpaces
+
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8083/eval/calcWinner", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String].flatMap { json =>
+            decode[List[Player]](json) match {
+              case Right(players) => Future.successful(players)
+              case Left(err)      => Future.failed(new RuntimeException(s"JSON decoding error: $err"))
+            }
+          }
+        case _ =>
+          Future.failed(new RuntimeException(s"calcWinner Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  def evalHand(
+      gameState: GameState,
+      player: Int
+  ): Future[String] = {
+
+    val jsonString = Map(
+      "gameState" -> gameState.asJson,
+      "player" -> player.asJson
+    ).asJson.noSpaces
+
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8083/eval/evalHand", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String]
+        case _ =>
+          Future.failed(new RuntimeException(s"evalHand Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  def getGUIView(
+      gameState: GameState,
+      handEval: String
+  ): Future[String] = {
+
+    val jsonString = Map(
+      "gameState" -> gameState.asJson,
+      "handEval" -> handEval.asJson
+    ).asJson.noSpaces
+
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8081/gui/getGUIView", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String]
+        case _ =>
+          Future.failed(new RuntimeException(s"getGUIView Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  def getTUIView(
+      gameState: GameState
+  ): Future[String] = {
+
+    val jsonString = gameState.asJson.noSpaces
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8082/tui/getTUIView", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String]
+        case _ =>
+          Future.failed(new RuntimeException(s"getTUIView Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  case class PlayerBalance(playerID: String, balance: Int)
+  def fetchBalance(playerID: String)(implicit system: ActorSystem, mat: Materializer): Future[PlayerBalance] = {
+    val jsonString = Map("playerID" -> playerID.asJson).asJson.noSpaces
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8084/db/fetchBalance", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String].flatMap { responseBody =>
+            decode[PlayerBalance](responseBody) match {
+              case Right(playerBalance) => Future.successful(playerBalance)
+              case Left(error)          => Future.failed(new RuntimeException(s"Invalid JSON response: ${error.getMessage}"))
+            }
+          }
+        case _ =>
+          Unmarshal(response.entity).to[String].flatMap { errorBody =>
+            Future.failed(new RuntimeException(s"fetchBalance failed with status ${response.status}: $errorBody"))
+          }
+      }
+    }
+  }
+
+  def updateBalance(
+      playerID: String,
+      balance: Int
+  ): Future[String] = {
+
+    val jsonString = Map(
+      "playerID" -> playerID.asJson,
+      "balance" -> balance.asJson
+    ).asJson.noSpaces
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8084/db/updateBalance", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String]
+        case _ =>
+          Future.failed(new RuntimeException(s"updateBalance Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  def insertPlayer(
+      playerID: String
+  ): Future[String] = {
+
+    val jsonString = Map("playerID" -> playerID.asJson).asJson.noSpaces
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8084/db/insertPlayer", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String]
+        case _ =>
+          Future.failed(new RuntimeException(s"insertPlayer Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  case class PlayerName(playerID: String, name: String)
+
+  def updateName(playerID: String, name: String): Future[String] = {
+    val jsonString = Map(
+      "playerID" -> playerID.asJson,
+      "name" -> name.asJson
+    ).asJson.noSpaces
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8084/db/updateName", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String]
+        case _ =>
+          Future.failed(new RuntimeException(s"updateName Failed with status ${response.status}"))
+      }
+    }
+  }
+
+  def fetchName(playerID: String)(implicit system: ActorSystem, mat: Materializer): Future[PlayerName] = {
+    val jsonString = Map("playerID" -> playerID.asJson).asJson.noSpaces
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8084/db/fetchName", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String].flatMap { responseBody =>
+            decode[PlayerName](responseBody) match {
+              case Right(playerName) => Future.successful(playerName)
+              case Left(error)       => Future.failed(new RuntimeException(s"Invalid JSON response: ${error.getMessage}"))
+            }
+          }
+        case _ =>
+          Unmarshal(response.entity).to[String].flatMap { errorBody =>
+            Future.failed(new RuntimeException(s"fetchName failed with status ${response.status}: $errorBody"))
+          }
+      }
+    }
+  }
+
+  case class InsertGameStateResponse(status: String, message: String)
+
+  def insertGameState(jsonString: String)(implicit system: ActorSystem, mat: Materializer): Future[InsertGameStateResponse] = {
+    val entity = HttpEntity(ContentTypes.`application/json`, jsonString)
+    val request = HttpRequest(HttpMethods.POST, "http://127.0.0.1:8084/db/insertGameState", entity = entity)
+
+    Http().singleRequest(request).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK =>
+          Unmarshal(response.entity).to[String].flatMap { responseBody =>
+            decode[InsertGameStateResponse](responseBody) match {
+              case Right(result) => Future.successful(result)
+              case Left(error)   => Future.failed(new RuntimeException(s"Invalid JSON response: ${error.getMessage}"))
+            }
+          }
+        case _ =>
+          Unmarshal(response.entity).to[String].flatMap { errorBody =>
+            Future.failed(new RuntimeException(s"insertGameState failed with status ${response.status}: $errorBody"))
+          }
+      }
+    }
+  }
 }

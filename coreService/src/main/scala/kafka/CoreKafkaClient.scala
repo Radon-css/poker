048894eb50ef624e4ce53d8874c8479101c53cfb
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2025-06-21 20:23:46 +0200

    Core Kafka

---
+++ coreService/src/main/scala/kafka/CoreKafkaClient.scala
@@ -0,0 +1,60 @@
+package de.htwg.poker.kafka
+
+import akka.actor.ActorSystem
+import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
+import akka.kafka.scaladsl.{Consumer, Producer}
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.{Materializer, OverflowStrategy}
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+import io.circe.parser._
+import java.util.UUID
+import scala.concurrent.{Future, Promise}
+import scala.collection.concurrent.TrieMap
+
+class CoreKafkaClient(implicit system: ActorSystem, mat: Materializer) {
+  import system.dispatcher
+
+  val bootstrapServers = "localhost:9092"
+  val responseTopic = "core-responses"
+
+  val promises = TrieMap[String, Promise[String]]()
+
+  val consumerSettings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
+    .withBootstrapServers(bootstrapServers)
+    .withGroupId("core-response-listener")
+    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+  val producerSettings = ProducerSettings(system, new StringSerializer, new StringSerializer)
+    .withBootstrapServers(bootstrapServers)
+
+  // Start response listener
+  Consumer
+    .plainSource(consumerSettings, Subscriptions.topics(responseTopic))
+    .mapAsync(1) { msg =>
+      decode[KafkaMessage](msg.value()) match {
+        case Right(kafkaMsg) =>
+          promises.remove(kafkaMsg.id).foreach(_.success(kafkaMsg.payload))
+        case Left(_) =>
+          system.log.warning(s"Invalid message received: ${msg.value()}")
+      }
+      Future.unit
+    }
+    .runWith(Sink.ignore)
+
+  def sendAndAwait(action: String, payload: String, targetTopic: String): Future[String] = {
+    val id = UUID.randomUUID().toString
+    val promise = Promise[String]()
+    promises.put(id, promise)
+
+    val message = KafkaMessage(id, action, payload, responseTopic).asJson.noSpaces
+    val record = new ProducerRecord[String, String](targetTopic, message)
+
+    Source
+      .single(record)
+      .runWith(Producer.plainSink(producerSettings))
+
+    promise.future
+  }
+}

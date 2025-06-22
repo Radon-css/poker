5a5cea63fb7d01e81b47cb27a2d37c76aa3c5ede
Radon-css <Julianschmuck@outlook.de>
2025-06-22 23:08:13 +0200

    more tests

--- coreService/src/test/scala/util/UpdateBoardSpec.scala
+++ coreService/src/test/scala/util/UpdateBoardSpec.scala
@@ -35,7 +35,6 @@ class UpdateBoardSpec extends AnyWordSpec with Matchers {
     bigBlind = 20
   )
 
-  // Test implementation without MockClient since we can't access the real Client
   "UpdateBoard" should {
     "select the correct strategy based on board size" in {
       val preFlopState = initialGameState.copy(board = Nil)
@@ -74,10 +73,88 @@ class UpdateBoardSpec extends AnyWordSpec with Matchers {
     }
 
     "handle startRound correctly" in {
-      // Testing without mocking the Client
       val resultState = Try(Await.result(UpdateBoard.startRound(initialGameState), 1.second))
       resultState.isSuccess shouldBe false
     }
+  }
 
+  "Balance updates" should {
+    "be calculated correctly for single winner" in {
+      val testState = initialGameState.copy(pot = 100)
+      val winnerName = "Player1"
+
+      // Simulate what would happen with a single winner
+      val expectedBalances = testState.playersAndBalances.map {
+        case (name, balance) if name == winnerName =>
+          (name, balance + 100 - (if (name == "Player1") 20 else 10))
+        case player => player
+      }
+
+      expectedBalances.find(_._1 == winnerName).get._2 shouldBe (1000 + 100 - 20)
+    }
+
+    "be calculated correctly for multiple winners" in {
+      val testState = initialGameState.copy(pot = 100)
+      val winningAmount = testState.pot / 2
+
+      val expectedBalances = testState.playersAndBalances.map {
+        case ("Player1", balance) => ("Player1", balance + winningAmount - 20)
+        case ("Player2", balance) => ("Player2", balance + winningAmount - 10)
+        case player               => player
+      }
+
+      expectedBalances.find(_._1 == "Player1").get._2 shouldBe (1000 + 50 - 20)
+      expectedBalances.find(_._1 == "Player2").get._2 shouldBe (1000 + 50 - 10)
+    }
+
+    "handle blind deductions correctly" in {
+      val testState = initialGameState.copy(pot = 100)
+
+      val expectedBalances = testState.playersAndBalances.map {
+        case ("Player1", balance) => ("Player1", balance - 20) // Small blind
+        case ("Player2", balance) => ("Player2", balance - 10) // Big blind
+        case player               => player
+      }
+
+      expectedBalances.find(_._1 == "Player1").get._2 shouldBe (1000 - 20)
+      expectedBalances.find(_._1 == "Player2").get._2 shouldBe (1000 - 10)
+    }
+  }
+
+  "New round initialization" should {
+    "have correct properties" in {
+      val testState = initialGameState.copy(pot = 100)
+
+      // This is what we expect the new round to look like
+      val expectedState = testState.copy(
+        players = Some(testPlayers),
+        deck = Some(testDeck.drop(4)),
+        playerAtTurn = 2,
+        currentHighestBetSize = 20,
+        board = Nil,
+        pot = 30,
+        smallBlindPointer = 1,
+        newRoundStarted = true
+      )
+
+      expectedState.board shouldBe Nil
+      expectedState.pot shouldBe 30
+      expectedState.currentHighestBetSize shouldBe 20
+      expectedState.newRoundStarted shouldBe true
+      expectedState.smallBlindPointer shouldBe 1
+      expectedState.playerAtTurn shouldBe 2
+      expectedState.deck.get.size shouldBe testDeck.size - 4
+    }
+  }
+
+  "Edge cases" should {
+    "handle zero balances correctly" in {
+      val zeroBalanceState = initialGameState.copy(
+        playersAndBalances = List(("Player1", 0), ("Player2", 0))
+      )
+
+      // Just verify the state is valid
+      zeroBalanceState.playersAndBalances.foreach(_._2 shouldBe 0)
+    }
   }
 }
